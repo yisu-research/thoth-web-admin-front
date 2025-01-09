0f22c3f23bdde357b0223643fde75b4b8f6f668d
yanglindong0725 <1009444100@qq.com>
2025-01-09 20:33:02 +0800

    style: update vercel.json

--- service.config.ts
+++ service.config.ts
@@ -7,6 +7,6 @@ export const serviceConfig: Record<ServiceEnvType, Record<string, string>> = {
     url: 'http://8.142.167.132:3002/api/',
   },
   prod: {
-    url: 'http://8.142.167.132:3002/api/',
+    url: '/api',
   },
 }
